f624981a171e3e4cb122dddb7b5de5d64778cbe8
Merche <merche.merino@iesjulianmarias.es>
2019-04-23 13:04:56 +0200

    ProyectoInicialmenteVacio

---
+++ src/ejerciciomodas2019/EjercicioModas2019.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciomodas2019;
+
+/**
+ *
+ * @author Administrador
+ */
+public class EjercicioModas2019 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
